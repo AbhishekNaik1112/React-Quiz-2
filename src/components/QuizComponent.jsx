b69bb53beaa4d822b04b88ff93fe1d7a4aa327ba
Abhishek Naik <142667674+AbhishekNaik1112@users.noreply.github.com>
2023-12-21 16:18:46 +0530

    Add files via upload

---
+++ src/components/QuizComponent.jsx
@@ -0,0 +1,76 @@
+import "./QuizComponent.css";
+import { useState } from "react";
+import data from "../assets/quizQuestion.json";
+
+function QuizComponent({ setState }) {
+  const [count, setCount] = useState(0);
+
+  const previous = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const next = () => {
+    if (count < 14) {
+      setCount(count + 1);
+    }
+  };
+
+  const handleOptionClick = (selectedOption) => {
+    console.log(`Option ${selectedOption} clicked`);
+    next();//will be given in <quiz-3>
+  };
+  
+ 
+  return (
+    <div className="container">
+      <div className="box">
+        <h1>Question</h1>
+        <p id="qnum">
+          {count + 1} of {15}
+        </p>
+        <h2 className="question" key={count}>
+          {data[count].question}
+        </h2>
+
+        <div className="options">
+          <button className="option" onClick={() => handleOptionClick('A')}>
+            {data[count].optionA}
+          </button>
+          <button className="option" onClick={() => handleOptionClick('B')}>
+            {data[count].optionB}
+          </button>
+          <button className="option" onClick={() => handleOptionClick('C')}>
+            {data[count].optionC}
+          </button>
+          <button className="option" onClick={() => handleOptionClick('D')}>
+            {data[count].optionD}
+          </button>
+        </div>
+
+        <div className="nextOptions">
+          <button className="previous" onClick={previous}>
+            Previous
+          </button>
+          <button className="next" onClick={next}>
+            Next
+          </button>
+          <button
+            onClick={() => {
+              const con = window.confirm("Are you sure you want to quit ?");
+              if (con) {
+                setState(3);
+              }
+            }}
+            className="quit"
+          >
+            Quit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default QuizComponent;
---
+++ src/components/ResultComponent.jsx
@@ -0,0 +1,73 @@
+import "./ResultComponent.css";
+
+function ResultComponent({ setState }) {
+  return (
+    <div className="container">
+      <h1
+        style={{
+          color: "#4CAF50",
+        }}
+      >
+        Result
+      </h1>
+      <div className="card">
+        <h3 id="phrase">You need more practice</h3>
+        <h1 id="score" style={{ color: "#40E0D0" }}>
+          Your Score is {20}%
+        </h1>
+        <div className="details">
+          <b>
+            <p>Total number of question</p>
+          </b>
+          <b>
+            <p>15</p>
+          </b>
+        </div>
+        <div className="details">
+          <b>
+            <p>No of attempted questions</p>
+          </b>
+          <b>
+            <p>9</p>
+          </b>
+        </div>
+        <div className="details">
+          <b>
+            <p>Number of correct answers</p>
+          </b>
+          <b>
+            <p>3</p>
+          </b>
+        </div>
+        <div className="details">
+          <b>
+            <p>Number of wrong answers</p>
+          </b>
+          <b>
+            <p>6</p>
+          </b>
+        </div>
+      </div>
+      <div>
+        <button
+          id="again"
+          onClick={() => {
+            setState(2);
+          }}
+        >
+          Play Again
+        </button>
+        <button
+          id="home"
+          onClick={() => {
+            setState(1);
+          }}
+        >
+          Back to Home
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ResultComponent;
